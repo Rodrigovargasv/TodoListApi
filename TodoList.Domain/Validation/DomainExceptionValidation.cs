ba2cb13c5756df48c3cccbdccb91fafeafcaad9f
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-09-25 14:33:47 -0300

    Implementado métados para validação das entitdades

--- TodoList.Domain/Validation/DomainExceptionValidation.cs
+++ TodoList.Domain/Validation/DomainExceptionValidation.cs
@@ -1,7 +1,18 @@
 ﻿
 namespace TodoList.Domain.Validation
 {
-    public class DomainExceptionValidation
+    public class DomainExceptionValidation : Exception
     {
+        public DomainExceptionValidation(string error) : base(error) { }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(error);
+            }
+        }
+        
+        
     }
 }
