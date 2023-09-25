62bd2d5e7b0ca5886018748a488073d518b501ef
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-09-25 15:28:05 -0300

    Configurado a fluent api para realizar as migrações

---
+++ TodoList.Infra.Data/EntityConfiguration/JobConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Infra.Data.EntityConfiguration
+{
+    public class JobConfiguration : IEntityTypeConfiguration<Job>
+    {
+
+        public void Configure(EntityTypeBuilder<Job> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Date).IsRequired();
+        }
+    }
+}
