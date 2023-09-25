9b18f0e87102f8c37bdb3eaf44df3a915e5d0243
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-09-25 14:17:38 -0300

    Criado o projeto TodoList, e a entidade Task com suas propriedades

---
+++ TodoList.Domain/Entities/Task.cs
@@ -0,0 +1,32 @@
+﻿
+namespace TodoList.Domain.Entities
+{
+    public class Task
+    {
+        public Task(int id, string name, string description, DateTime createDate, DateTime executionDate, TaskStatus taskStatus)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            CreateDate = createDate;
+            ExecutionDate = executionDate;
+            this.taskStatus = taskStatus;
+        }
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime CreateDate { get; private set; }
+
+        public DateTime ExecutionDate { get; private set; }
+
+        public TaskStatus taskStatus { get; }
+
+
+
+
+    }
+}
---
+++ TodoList.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TodoList.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        PENDENTE = 1,
+        Em_PROGRESSO = 2,
+        CONCLUIDA = 3
+    }
+}
