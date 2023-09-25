ce530bce2d146601282868255c7eac1cc531229e
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-09-25 18:29:41 -0300

    Criado a pasta Classe JobRepository e a interface IJobRepository e implementado os métado basicos para realização de uma CRUD.

---
+++ TodoList.Domain/Interfaces/IJobRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using TodoList.Domain.Entities;
+
+namespace TodoList.Domain.Interfaces
+{
+    public interface IJobRepository
+    {
+        Task<Job> CreateJobAync(Job job);
+        Task<IEnumerable<Job>> GetAllJobAsync();
+        Task<Job> GetJobByIdAsync(int? id);
+        Task<Job> UpdateJobAsync(Job job);
+        Task<Job> DeleteJobAsync(Job job);
+
+
+
+    }
+}
---
+++ TodoList.Infra.Data/Repository/JobRepository.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TodoList.Domain.Entities;
+using TodoList.Domain.Interfaces;
+using TodoList.Infra.Data.Context;
+
+namespace TodoList.Infra.Data.Repository
+{
+    public class JobRepository : IJobRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public JobRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Job> CreateJobAync(Job job)
+        {
+            _context.Add(job);
+            await _context.SaveChangesAsync();
+            return job;
+        }
+
+        public async Task<Job> DeleteJobAsync(Job job)
+        {
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+            return job;
+        }
+
+        public async Task<IEnumerable<Job>> GetAllJobAsync()
+        {
+            return await _context.Jobs.ToListAsync();
+        }
+
+        public async Task<Job> GetJobByIdAsync(int? id)
+        {
+           return await _context.Jobs.FindAsync(id);
+            
+        }
+
+        public async Task<Job> UpdateJobAsync(Job job)
+        {
+            _context.Jobs.Update(job);
+            await _context.SaveChangesAsync();
+
+            return job;
+        }
+    }
+}
