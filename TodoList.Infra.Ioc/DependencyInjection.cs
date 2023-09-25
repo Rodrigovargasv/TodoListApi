fee49e89b92ab99f8153c4a0e5ed49498c2da119
Rodrigovargasv <rodrigovargasv80@gmail.com>
2023-09-25 16:20:55 -0300

    Criado classe DependencyInjection.

---
+++ TodoList.Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TodoList.Infra.Data.Context;
+
+namespace TodoList.Infra.Ioc
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(option =>
+            option.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+            bulder => bulder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            return services;
+        }
+
+    }
+}
